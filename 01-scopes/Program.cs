204f777d290ef01851d86c8b6d1ed4a64a2c1914
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-09-25 12:00:24 +0200

    First code
    Output is correct, but validation is needed to be added.

--- 01-scopes/Program.cs
+++ 01-scopes/Program.cs
@@ -1,5 +1,31 @@
-﻿int i = 5;
-Console.WriteLine(i++); // 5
-Console.WriteLine(i); // 6
-Console.WriteLine(i++); // 
-Console.WriteLine(i); // 
+﻿
+{
+Console.WriteLine("Give me a number");
+string? userInput = Console.ReadLine();
+int parsedInput;
+while (!int.TryParse(userInput, out parsedInput))
+    
+    {
+        Console.WriteLine("Wrong, give me a numbers and no characters.");
+        userInput = Console.ReadLine();
+    
+    }
+    int intUserInput = Convert.ToInt32(userInput);
+    Console.WriteLine(intUserInput+82);
+}
+
+{
+Console.WriteLine("Give me a another number");
+int userInput = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(++userInput);
+}
+
+{
+    
+    Console.WriteLine("Give me a another number");
+    string userInput = Console.ReadLine();
+    userInput = ":";
+    Console.WriteLine(userInput);
+    
+    
+}
--- Template/Program.cs
+++ Template/Program.cs
@@ -1,5 +1,1 @@
-﻿int i = 5;
-Console.WriteLine(i++); // 5
-Console.WriteLine(i); // 6
-Console.WriteLine(i++); // 
-Console.WriteLine(i); // 
+﻿
