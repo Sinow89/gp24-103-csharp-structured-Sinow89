e75a17b7a373a2b23b20a4f5807a6c12af4e10d9
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-09-28 20:53:39 +0200

    Program done

--- 02-functions/Program.cs
+++ 02-functions/Program.cs
@@ -1,13 +1,41 @@
 ﻿
 void moveForward()
-{
-    Console.WriteLine("Move Forward.");
-
-}
-
+{ Console.WriteLine("Move Forward."); }
 void turnRight()
-{
-    Console.WriteLine("Turn Right.");
+{ Console.WriteLine("Turn Right."); }
 
+{
+    for (int i = 0; i < 3; i++)
+        moveForward();
+}
+{
+    for (int i = 0; i < 2; i++) 
+        turnRight();
+}
+{
+    for (int i = 0; i < 3; i++)
+        moveForward();
+}
+{
+    for (int i = 0; i < 3; i++) 
+        turnRight();
+}{
+    for (int i = 0; i < 3; i++)
+        moveForward();
+}
+{
+    for (int i = 0; i < 3; i++) 
+        turnRight();
+}{
+    for (int i = 0; i < 1; i++)
+        moveForward();
+}
+{
+    for (int i = 0; i < 2; i++)
+        turnRight();
+}
+{
+    for (int i = 0; i < 1; i++)
+        moveForward();
 }
 
